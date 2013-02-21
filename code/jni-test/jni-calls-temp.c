ccb1d5e11ca50d2f52977b1b6e1521ae618bef1b
Timo Tuominen <tituomin@gmail.com>
2013-02-22 01:23:46 +0200

    Funktioita lajiteltu.

--- code/jni-test/jni-calls-temp.c
+++ code/jni-test/jni-calls-temp.c
@@ -1,72 +1,118 @@
 
-static jint GetVersion(JNIEnv* env)
-static jclass DefineClass(JNIEnv* env, const char *name, jobject loader, const jbyte* buf, jsize bufLen)
-static jclass FindClass(JNIEnv* env, const char* name)
-static jclass GetSuperclass(JNIEnv* env, jclass jclazz)
-static jboolean IsAssignableFrom(JNIEnv* env, jclass jclazz1, jclass jclazz2)
-static jmethodID FromReflectedMethod(JNIEnv* env, jobject jmethod)
-static jfieldID FromReflectedField(JNIEnv* env, jobject jfield)
-static jobject ToReflectedMethod(JNIEnv* env, jclass jcls, jmethodID methodID, jboolean isStatic)
-static jobject ToReflectedField(JNIEnv* env, jclass jcls, jfieldID fieldID, jboolean isStatic)
-static jint Throw(JNIEnv* env, jthrowable jobj)
-static jint ThrowNew(JNIEnv* env, jclass jclazz, const char* message)
-static jthrowable ExceptionOccurred(JNIEnv* env)
-static void ExceptionDescribe(JNIEnv* env)
-static void ExceptionClear(JNIEnv* env)
-static void FatalError(JNIEnv* env, const char* msg)
-static jint PushLocalFrame(JNIEnv* env, jint capacity)
-static jobject PopLocalFrame(JNIEnv* env, jobject jresult)
-static jobject NewGlobalRef(JNIEnv* env, jobject jobj)
-static void DeleteGlobalRef(JNIEnv* env, jobject jglobalRef)
-static jobject NewLocalRef(JNIEnv* env, jobject jobj)
-static void DeleteLocalRef(JNIEnv* env, jobject jlocalRef)
-static jint EnsureLocalCapacity(JNIEnv* env, jint capacity)
-static jboolean IsSameObject(JNIEnv* env, jobject jref1, jobject jref2)
-static jobject AllocObject(JNIEnv* env, jclass jclazz)
-static jobject NewObject(JNIEnv* env, jclass jclazz, jmethodID methodID, ...)
-static jobject NewObjectV(JNIEnv* env, jclass jclazz, jmethodID methodID, va_list args)
-static jobject NewObjectA(JNIEnv* env, jclass jclazz, jmethodID methodID, jvalue* args)
-static jclass GetObjectClass(JNIEnv* env, jobject jobj)
-static jboolean IsInstanceOf(JNIEnv* env, jobject jobj, jclass jclazz)
-static jmethodID GetMethodID(JNIEnv* env, jclass jclazz, const char* name, const char* sig)
-static jfieldID GetFieldID(JNIEnv* env, jclass jclazz, const char* name, const char* sig)
-static jmethodID GetStaticMethodID(JNIEnv* env, jclass jclazz, const char* name, const char* sig)
-static jfieldID GetStaticFieldID(JNIEnv* env, jclass jclazz, const char* name, const char* sig)
-static jstring NewString(JNIEnv* env, const jchar* unicodeChars, jsize len)
-static jsize GetStringLength(JNIEnv* env, jstring jstr)
-static const jchar* GetStringChars(JNIEnv* env, jstring jstr, jboolean* isCopy)
-static void ReleaseStringChars(JNIEnv* env, jstring jstr, const jchar* chars)
-static jstring NewStringUTF(JNIEnv* env, const char* bytes)
-static jsize GetStringUTFLength(JNIEnv* env, jstring jstr)
-static const char* GetStringUTFChars(JNIEnv* env, jstring jstr, jboolean* isCopy)
-static void ReleaseStringUTFChars(JNIEnv* env, jstring jstr, const char* utf)
-static jsize GetArrayLength(JNIEnv* env, jarray jarr)
-static jobjectArray NewObjectArray(JNIEnv* env, jsize length, jclass jelementClass, jobject jinitialElement)
-static bool checkArrayElementBounds(ArrayObject* arrayObj, jsize index)
-static jobject GetObjectArrayElement(JNIEnv* env, jobjectArray jarr, jsize index)
-static void SetObjectArrayElement(JNIEnv* env, jobjectArray jarr, jsize index, jobject jobj)
-static void throwArrayRegionOutOfBounds(ArrayObject* arrayObj, jsize start, jsize len, const char* arrayIdentifier)
-static jint RegisterNatives(JNIEnv* env, jclass jclazz, const JNINativeMethod* methods, jint nMethods)
-static jint UnregisterNatives(JNIEnv* env, jclass jclazz)
-static jint MonitorEnter(JNIEnv* env, jobject jobj)
-static jint MonitorExit(JNIEnv* env, jobject jobj)
-static jint GetJavaVM(JNIEnv* env, JavaVM** vm)
-static void GetStringRegion(JNIEnv* env, jstring jstr, jsize start, jsize len, jchar* buf)
-static void GetStringUTFRegion(JNIEnv* env, jstring jstr, jsize start, jsize len, char* buf)
-static void* GetPrimitiveArrayCritical(JNIEnv* env, jarray jarr, jboolean* isCopy)
-static void ReleasePrimitiveArrayCritical(JNIEnv* env, jarray jarr, void* carray, jint mode)
-static const jchar* GetStringCritical(JNIEnv* env, jstring jstr, jboolean* isCopy)
-static void ReleaseStringCritical(JNIEnv* env, jstring jstr, const jchar* carray)
-static jweak NewWeakGlobalRef(JNIEnv* env, jobject jobj)
-static void DeleteWeakGlobalRef(JNIEnv* env, jweak wref)
-static jboolean ExceptionCheck(JNIEnv* env)
-static jobjectRefType GetObjectRefType(JNIEnv* env, jobject jobj)
-static jobject NewDirectByteBuffer(JNIEnv* env, void* address, jlong capacity)
-static void* GetDirectBufferAddress(JNIEnv* env, jobject jbuf)
-static jlong GetDirectBufferCapacity(JNIEnv* env, jobject jbuf)
-static jint attachThread(JavaVM* vm, JNIEnv** p_env, void* thr_args, bool isDaemon)
-static jint AttachCurrentThread(JavaVM* vm, JNIEnv** p_env, void* thr_args)
-static jint AttachCurrentThreadAsDaemon(JavaVM* vm, JNIEnv** p_env, void* thr_args)
-static jint DetachCurrentThread(JavaVM* vm)
-static jint GetEnv(JavaVM* vm, void** env, jint version)
-static jint DestroyJavaVM(JavaVM* vm)
+/* puuttuu!!! makroversiot...)
+
+/* 1 */
+jboolean       ExceptionCheck (                JNIEnv* env )
+jint           GetVersion (                    JNIEnv* env )
+jthrowable     ExceptionOccurred (             JNIEnv* env )
+void           ExceptionClear (                JNIEnv* env )
+void           ExceptionDescribe (             JNIEnv* env )
+
+/* 2 */
+void           DeleteGlobalRef (               JNIEnv* env,           jobject jglobalRef )
+void           DeleteLocalRef (                JNIEnv* env,           jobject jlocalRef )
+void           DeleteWeakGlobalRef (           JNIEnv* env,           jweak wref )
+void           FatalError (                    JNIEnv* env,           const char* msg )
+
+jint           EnsureLocalCapacity (           JNIEnv* env,           jint capacity )
+jint           GetJavaVM (                     JNIEnv* env,           JavaVM** vm )
+jint           MonitorEnter (                  JNIEnv* env,           jobject jobj )
+jint           MonitorExit (                   JNIEnv* env,           jobject jobj )
+jint           PushLocalFrame (                JNIEnv* env,           jint capacity )
+jint           Throw (                         JNIEnv* env,           jthrowable jobj )
+jint           UnregisterNatives (             JNIEnv* env,           jclass jclazz )
+jlong          GetDirectBufferCapacity (       JNIEnv* env,           jobject jbuf )
+jsize          GetArrayLength (                JNIEnv* env,           jarray jarr )
+jsize          GetStringLength (               JNIEnv* env,           jstring jstr )
+jsize          GetStringUTFLength (            JNIEnv* env,           jstring jstr )
+
+jobject        AllocObject (                   JNIEnv* env,           jclass jclazz )
+jobject        NewGlobalRef (                  JNIEnv* env,           jobject jobj )
+jobject        NewLocalRef (                   JNIEnv* env,           jobject jobj )
+jobject        PopLocalFrame (                 JNIEnv* env,           jobject jresult )
+jclass         FindClass (                     JNIEnv* env,           const char* name )
+jclass         GetObjectClass (                JNIEnv* env,           jobject jobj )
+jclass         GetSuperclass (                 JNIEnv* env,           jclass jclazz )
+
+jstring        NewStringUTF (                  JNIEnv* env,           const char* bytes )
+
+jfieldID       FromReflectedField (            JNIEnv* env,           jobject jfield )
+jmethodID      FromReflectedMethod (           JNIEnv* env,           jobject jmethod )
+
+jobjectRefType GetObjectRefType (              JNIEnv* env,           jobject jobj )
+
+jweak          NewWeakGlobalRef (              JNIEnv* env,           jobject jobj )
+
+void*          GetDirectBufferAddress (        JNIEnv* env,           jobject jbuf )
+
+/* 3 */
+const char*    GetStringUTFChars (             JNIEnv* env,           jstring jstr,              jboolean* isCopy )
+const jchar*   GetStringChars (                JNIEnv* env,           jstring jstr,              jboolean* isCopy )
+const jchar*   GetStringCritical (             JNIEnv* env,           jstring jstr,              jboolean* isCopy )
+jboolean       IsAssignableFrom (              JNIEnv* env,           jclass jclazz1,            jclass jclazz2 )
+jboolean       IsInstanceOf (                  JNIEnv* env,           jobject jobj,              jclass jclazz )
+jboolean       IsSameObject (                  JNIEnv* env,           jobject jref1,             jobject jref2 )
+jint           ThrowNew (                      JNIEnv* env,           jclass jclazz,             const char* message )
+jobject        GetObjectArrayElement (         JNIEnv* env,           jobjectArray jarr,         jsize index )
+jobject        NewDirectByteBuffer (           JNIEnv* env,           void* address,             jlong capacity )
+jstring        NewString (                     JNIEnv* env,           const jchar* unicodeChars, jsize len )
+void           ReleaseStringChars (            JNIEnv* env,           jstring jstr,              const jchar* chars )
+void           ReleaseStringCritical (         JNIEnv* env,           jstring jstr,              const jchar* carray )
+void           ReleaseStringUTFChars (         JNIEnv* env,           jstring jstr,              const char* utf )
+void*          GetPrimitiveArrayCritical (     JNIEnv* env,           jarray jarr,               jboolean* isCopy )
+
+/* 4 */
+jfieldID       GetFieldID (                    JNIEnv* env,           jclass jclazz,             const char* name,               const char* sig )
+jfieldID       GetStaticFieldID (              JNIEnv* env,           jclass jclazz,             const char* name,               const char* sig )
+jint           RegisterNatives (               JNIEnv* env,           jclass jclazz,             const JNINativeMethod* methods, jint nMethods )
+jint           attachThread (                  JavaVM* vm,            JNIEnv** p_env,            void* thr_args,                 bool isDaemon )
+jmethodID      GetMethodID (                   JNIEnv* env,           jclass jclazz,             const char* name,               const char* sig )
+jmethodID      GetStaticMethodID (             JNIEnv* env,           jclass jclazz,             const char* name,               const char* sig )
+jobject        ToReflectedField (              JNIEnv* env,           jclass jcls,               jfieldID fieldID,               jboolean isStatic )
+jobject        ToReflectedMethod (             JNIEnv* env,           jclass jcls,               jmethodID methodID,             jboolean isStatic )
+jobjectArray   NewObjectArray (                JNIEnv* env,           jsize length,              jclass jelementClass,           jobject jinitialElement )
+void           ReleasePrimitiveArrayCritical ( JNIEnv* env,           jarray jarr,               void* carray,                   jint mode )
+void           SetObjectArrayElement (         JNIEnv* env,           jobjectArray jarr,         jsize index,                    jobject jobj )
+void           throwArrayRegionOutOfBounds (   ArrayObject* arrayObj, jsize start,               jsize len,                      const char* arrayIdentifier )
+
+/* 5 */
+jclass         DefineClass (                   JNIEnv* env,           const char *name,          jobject loader,                 const jbyte* buf, jsize bufLen )
+void           GetStringRegion (               JNIEnv* env,           jstring jstr,              jsize start,                    jsize len,        jchar* buf )
+void           GetStringUTFRegion (            JNIEnv* env,           jstring jstr,              jsize start,                    jsize len,        char* buf )
+
+jobject        NewObject (                     JNIEnv* env,           jclass jclazz,             jmethodID methodID,             ... )
+jobject        NewObjectV (                    JNIEnv* env,           jclass jclazz,             jmethodID methodID,             va_list args )
+jobject        NewObjectA (                    JNIEnv* env,           jclass jclazz,             jmethodID methodID,             jvalue* args )
+
+/* -- */
+
+jint           AttachCurrentThread (           JavaVM* vm,            JNIEnv** p_env,            void* thr_args )
+jint           AttachCurrentThreadAsDaemon (   JavaVM* vm,            JNIEnv** p_env,            void* thr_args )
+jint           GetEnv (                        JavaVM* vm,            void** env,                jint version )
+
+jint           DetachCurrentThread (           JavaVM* vm )
+jint           DestroyJavaVM (                 JavaVM* vm )
+
+
+// makrot:
+/* GET_STATIC_TYPE_FIELD(_ctype, _jname, _isref) */
+/* SET_STATIC_TYPE_FIELD(_ctype, _ctype2, _jname, _isref) */
+/* GET_TYPE_FIELD(_ctype, _jname, _isref) */
+/* SET_TYPE_FIELD(_ctype, _ctype2, _jname, _isref) */
+
+/* CALL_VIRTUAL(_ctype, _jname, _retfail, _retok, _isref) */
+/* CALL_NONVIRTUAL(_ctype, _jname, _retfail, _retok, _isref) */
+/* CALL_STATIC(_ctype, _jname, _retfail, _retok, _isref) */
+
+/* NEW_PRIMITIVE_ARRAY(_artype, _jname, _typechar) */
+
+/* GET_PRIMITIVE_ARRAY_ELEMENTS(_ctype, _jname) */
+/* RELEASE_PRIMITIVE_ARRAY_ELEMENTS(_ctype, _jname) */
+/* GET_PRIMITIVE_ARRAY_REGION(_ctype, _jname) */
+/* SET_PRIMITIVE_ARRAY_REGION(_ctype, _jname) */
+
+/* PRIMITIVE_ARRAY_FUNCTIONS(_ctype, _jname) */
+
+// idea: poimi makroilla luodut versiot 
+// kääntäjän preprosessoritulostuksesta
+
--- code/jni-test/jni-mock.c
+++ code/jni-test/jni-mock.c
@@ -9,10 +9,10 @@ static const jchar mock_jstring[10];
 struct _jfieldID {  int id; };
 struct _jmethodID {  int id; };
 static struct _jfieldID mock_fid = {
-  5
+    5
 };
 static struct _jmethodID mock_mid = {
-  5
+    5
 };
 
 static void* void_ptr = (void *) "MOCK_STRING";
@@ -27,210 +27,210 @@ static void* void_ptr = (void *) "MOCK_STRING";
 /* ------- mock implementation ----- */
 
 static const char* GetStringUTFChars(JNIEnv* env, jstring jstr, jboolean* isCopy) {
-  return mock_string;
+    return mock_string;
 }
 static const jchar* GetStringChars(JNIEnv* env, jstring jstr, jboolean* isCopy) {
-  return mock_jstring;
+    return mock_jstring;
 }
 static const jchar* GetStringCritical(JNIEnv* env, jstring jstr, jboolean* isCopy) {
-  return mock_jstring;
+    return mock_jstring;
 }
 static jboolean ExceptionCheck(JNIEnv* env) {
-  return JBOOLEAN_V;
+    return JBOOLEAN_V;
 }
 static jboolean IsAssignableFrom(JNIEnv* env, jclass jclazz1, jclass jclazz2) {
-  return JBOOLEAN_V;
+    return JBOOLEAN_V;
 }
 static jboolean IsInstanceOf(JNIEnv* env, jobject jobj, jclass jclazz) {
-  return JBOOLEAN_V;
+    return JBOOLEAN_V;
 }
 static jboolean IsSameObject(JNIEnv* env, jobject jref1, jobject jref2) {
-  return JBOOLEAN_V;
+    return JBOOLEAN_V;
 }
 static jint AttachCurrentThread(JavaVM* vm, JNIEnv** p_env, void* thr_args) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint AttachCurrentThreadAsDaemon(JavaVM* vm, JNIEnv** p_env, void* thr_args){
-  return JINT_V;
+    return JINT_V;
 }
 static jint DestroyJavaVM(JavaVM* vm) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint DetachCurrentThread(JavaVM* vm) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint EnsureLocalCapacity(JNIEnv* env, jint capacity) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint GetEnv(JavaVM* vm, void** env, jint version) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint GetJavaVM(JNIEnv* env, JavaVM** vm) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint GetVersion(JNIEnv* env) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint MonitorEnter(JNIEnv* env, jobject jobj) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint MonitorExit(JNIEnv* env, jobject jobj) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint PushLocalFrame(JNIEnv* env, jint capacity) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint RegisterNatives(JNIEnv* env, jclass jclazz, const JNINativeMethod* methods, jint nMethods){
-  return JINT_V;
+    return JINT_V;
 }
 static jint Throw(JNIEnv* env, jthrowable jobj) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint ThrowNew(JNIEnv* env, jclass jclazz, const char* message) {
-  return JINT_V;
+    return JINT_V;
 }
 static jint UnregisterNatives(JNIEnv* env, jclass jclazz) {
-  return JINT_V;
+    return JINT_V;
 }
 static jlong GetDirectBufferCapacity(JNIEnv* env, jobject jbuf) {
-  return JLONG_V;
+    return JLONG_V;
 }
 static jsize GetArrayLength(JNIEnv* env, jarray jarr) {
-  return JSIZE_V;
+    return JSIZE_V;
 }
 static jsize GetStringLength(JNIEnv* env, jstring jstr) {
-  return JSIZE_V;
+    return JSIZE_V;
 }
 static jsize GetStringUTFLength(JNIEnv* env, jstring jstr) {
-  return JSIZE_V;
+    return JSIZE_V;
 }
 static jmethodID FromReflectedMethod(JNIEnv* env, jobject jmethod) {
-  return &mock_mid;
+    return &mock_mid;
 }
 static jmethodID GetMethodID(JNIEnv* env, jclass jclazz, const char* name, const char* sig) {
-  return &mock_mid;
+    return &mock_mid;
 }
 static jmethodID GetStaticMethodID(JNIEnv* env, jclass jclazz, const char* name, const char* sig) {
-  return &mock_mid;
+    return &mock_mid;
 }
 static jfieldID FromReflectedField(JNIEnv* env, jobject jfield) {
-  return &mock_fid;
+    return &mock_fid;
 }
 static jfieldID GetFieldID(JNIEnv* env, jclass jclazz, const char* name, const char* sig) {
-  return &mock_fid;
+    return &mock_fid;
 }
 static jfieldID GetStaticFieldID(JNIEnv* env, jclass jclazz, const char* name, const char* sig) {
-  return &mock_fid;
+    return &mock_fid;
 }
 static jobject AllocObject(JNIEnv* env, jclass jclazz) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject GetObjectArrayElement(JNIEnv* env, jobjectArray jarr, jsize index) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject NewDirectByteBuffer(JNIEnv* env, void* address, jlong capacity) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject NewGlobalRef(JNIEnv* env, jobject jobj) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject NewLocalRef(JNIEnv* env, jobject jobj) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject NewObject(JNIEnv* env, jclass jclazz, jmethodID methodID, ...) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject NewObjectA(JNIEnv* env, jclass jclazz, jmethodID methodID, jvalue* args) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject NewObjectV(JNIEnv* env, jclass jclazz, jmethodID methodID, va_list args) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject PopLocalFrame(JNIEnv* env, jobject jresult) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject ToReflectedField(JNIEnv* env, jclass jcls, jfieldID fieldID, jboolean isStatic) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobject ToReflectedMethod(JNIEnv* env, jclass jcls, jmethodID methodID, jboolean isStatic) {
-  return void_ptr;
+    return void_ptr;
 }
 static jclass DefineClass(JNIEnv* env, const char *name, jobject loader, const jbyte* buf, jsize bufLen){
-  return void_ptr;
+    return void_ptr;
 }
 static jclass FindClass(JNIEnv* env, const char* name) {
-  return void_ptr;
+    return void_ptr;
 }
 static jclass GetObjectClass(JNIEnv* env, jobject jobj) {
-  return void_ptr;
+    return void_ptr;
 }
 static jclass GetSuperclass(JNIEnv* env, jclass jclazz) {
-  return void_ptr;
+    return void_ptr;
 }
 static jobjectArray NewObjectArray(JNIEnv* env, jsize length, jclass jelementClass, jobject jinitialElement){
-  return void_ptr;
+    return void_ptr;
 }
 static jobjectRefType GetObjectRefType(JNIEnv* env, jobject jobj) {
-  return 1;
+    return 1;
 }
 static jstring NewString(JNIEnv* env, const jchar* unicodeChars, jsize len) {
-  return void_ptr;
+    return void_ptr;
 }
 static jstring NewStringUTF(JNIEnv* env, const char* bytes) {
-  return void_ptr;
+    return void_ptr;
 }
 static jthrowable ExceptionOccurred(JNIEnv* env) {
-  return void_ptr;
+    return void_ptr;
 }
 static jweak NewWeakGlobalRef(JNIEnv* env, jobject jobj) {
-  return void_ptr;
+    return void_ptr;
 }
 static void* GetDirectBufferAddress(JNIEnv* env, jobject jbuf) {
-  return void_ptr;
+    return void_ptr;
 }
 static void* GetPrimitiveArrayCritical(JNIEnv* env, jarray jarr, jboolean* isCopy) {
-  return void_ptr;
+    return void_ptr;
 }
 static void DeleteGlobalRef(JNIEnv* env, jobject jglobalRef) {
-  return;
+    return;
 }
 static void DeleteLocalRef(JNIEnv* env, jobject jlocalRef) {
-  return;
+    return;
 }
 static void DeleteWeakGlobalRef(JNIEnv* env, jweak wref) {
-  return;
+    return;
 }
 static void ExceptionClear(JNIEnv* env) {
-  return;
+    return;
 }
 static void ExceptionDescribe(JNIEnv* env) {
-  return;
+    return;
 }
 static void FatalError(JNIEnv* env, const char* msg) {
-  return;
+    return;
 }
 static void GetStringRegion(JNIEnv* env, jstring jstr, jsize start, jsize len, jchar* buf) {
-  return;
+    return;
 }
 static void GetStringUTFRegion(JNIEnv* env, jstring jstr, jsize start, jsize len, char* buf) {
-  return;
+    return;
 }
 static void ReleasePrimitiveArrayCritical(JNIEnv* env, jarray jarr, void* carray, jint mode) {
-  return;
+    return;
 }
 static void ReleaseStringChars(JNIEnv* env, jstring jstr, const jchar* chars) {
-  return;
+    return;
 }
 static void ReleaseStringCritical(JNIEnv* env, jstring jstr, const jchar* carray) {
-  return;
+    return;
 }
 static void ReleaseStringUTFChars(JNIEnv* env, jstring jstr, const char* utf) {
-  return;
+    return;
 }
 static void SetObjectArrayElement(JNIEnv* env, jobjectArray jarr, jsize index, jobject jobj) {
-  return;
+    return;
 }
 
 int main() {
-  return 1;
+    return 1;
 }
