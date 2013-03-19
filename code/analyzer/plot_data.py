d1d5c32be06943c5033860aab9fd12cd2a7088d1
Timo Tuominen <tituomin@gmail.com>
2013-03-19 16:37:38 +0200

    Added missing return_type field. Analyzer script working.

--- code/analyzer/plot_data.py
+++ code/analyzer/plot_data.py
@@ -4,29 +4,14 @@ from sys import argv
 import itertools
 import functools
 import re
+import pprint
+
+pp = pprint.PrettyPrinter(depth=10, indent=4)
+
 
 SEPARATOR = ','
 NUMERICAL = '[0-9]+'
 re_numerical = re.compile(NUMERICAL)
-
-dependencies = [
-    # if key (left) chosen as variable,
-    # corresponding keys (right) must be removed
-    # since they are not independent from the key
-    ('parameter_count'        , 'parameter_type_.+count'),
-    ('parameter_type_count'   , 'parameter_count'),
-    ('parameter_type_.+count' , 'parameter_count')]
-
-def remove_dependencies(series, sorted_keys, variable):
-    for benchmark in benchmarks:
-        for var, removable in dependencies:
-            if re.match(var, variable):
-                for key in benchmark.keys():
-                    if re.match(removable, key):
-                        del benchmark[key]
-                for key in sorted_keys:
-                    if re.match(removable, key):
-                        sorted_keys.remove(key)
 
 def explode(line):
     return line.split(SEPARATOR)
@@ -38,7 +23,7 @@ def value(string):
         return int(string)
     return string
 
-def read_datafile(f):
+def read_datafile(f, variable):
     benchmarks = []
     labels = explode(f.readline())[:-1]
     for line in f:
@@ -51,69 +36,103 @@ def read_datafile(f):
 def extract_data(benchmarks, group, variable, measure):
     series_collection = []
 
-    exclude_list = explode(group)
-    exclude_list.append(variable)
-    exclude_list.append(measure)
+    focus = explode(group)
+    focus.append(variable)
+    focus.append(measure)
+
+    additional_info = []
+    if re.match('parameter_type_.+count', variable):
+        additional_info.append('parameter_count')
+    additional_info.append('no')
+
+    exclude_from_sorting = []
+    exclude_from_sorting.extend(additional_info)
+    exclude_from_sorting.extend(focus)
 
     labels = benchmarks[0].keys()
-    sorted_keys = filter(lambda x: not x in exclude_list, labels)
-    sorted_keys.extend(exclude_list)
-
-    controlled_variables = filter(lambda x: not x in exclude_list, labels)
+    sorted_keys = filter(lambda x: not x in exclude_from_sorting, labels)
+    sorted_keys.extend(exclude_from_sorting)
 
     compare_function = functools.partial(comp_function, sorted_keys)
     sorted_benchmarks = sorted(benchmarks, cmp=compare_function)
 
     last_fixed = None
-    series = []
+    series = None
+    controlled_variables = filter(lambda x: not x in exclude_from_sorting, labels)
+
     for benchmark in sorted_benchmarks:
         fixed_data = tuple()
         for key in controlled_variables:
-            fixed_data += benchmark[key],
+            fixed_data += (key, benchmark[key]),
+
+        extra_data = tuple()
+        for key in additional_info:
+            extra_data += (key, benchmark[key]),
 
         element = {
-            'fixed' : fixed_data,
+            'fixed'  : fixed_data,
+            'info'   : extra_data,
             variable : benchmark[variable],
-            measure : benchmark[measure],
-            group : benchmark[group]}
+            measure  : benchmark[measure],
+            group    : benchmark[group]}
 
         if last_fixed == fixed_data:
-            series.append(element)
+            if (benchmark[group] not in series):
+                series[benchmark[group]] = []
+            series[benchmark[group]].append(element)
 
         else:
-            series_collection.append(series)
-            series = [element]
+            if (series != None):
+                append = True
+                for bms in series.values():
+                    if len(bms) < 2:
+                        append = False
+                if append:
+                    series_collection.append(series)
+            series = {benchmark[group]: [element]}
 
         last_fixed = fixed_data
         
-    return series_collection, sorted_keys
+    return series_collection
 
 def comp_function(keys, left, right):
     for key in keys:
-        l = left[key]
-        r = right[key]
-
+        l, r = left[key], right[key]
         if l == None:
-            return -1
-        if r == None:
             return 1
-        if type(l) == int:
-            if l < r:
-                return -1
-            if l > r:
-                return 1
-            if l == r:
-                return 0
+        if r == None:
+            return -1
+        if l < r:
+            return 1
+        if l > r:
+            return -1
+    return 0        
+            
 
-def print_benchmarks(data, sorted_keys):
-    i = 0
+def print_benchmarks(data, group, variable, measure):
     for series in data:
-        for benchmark in series:
-            if i == 0:
-                print ' '.join(benchmark.keys())
-            print ' '.join(benchmark.values())
-        print ' ---------- '
-
+        headers = []
+        headers.append(variable)
+        headers.extend(series.keys())
+        print '# ' + measure + " / " + variable
+        for header in headers:
+            print '"{label}"'.format(label=header),
+        print
+        var_value = None
+        group_vals = []
+        for idx in range(0, len(series.values()[0])):
+            i = 0
+            for grp in series.values():
+                if i == 0:
+                    var_value = grp[idx][variable]
+                    print var_value, 
+                elif var_value != grp[idx][variable]:
+                    print 'Error: groups have different variables'
+                    exit(1)
+                i += 1
+                print grp[idx][measure],
+            print
+        
 if __name__ == '__main__':
     if len(argv) != 5:
         print "\nUsage: python plot_data.py filename group variable measure"
@@ -126,14 +145,15 @@ if __name__ == '__main__':
 
     f = open(filename)
     try:
-        benchmarks = read_datafile(f)
+        benchmarks = read_datafile(f, variable)
     finally:
         f.close()
 
-    data, keys = extract_data(benchmarks, group, variable, measure)
-    remove_dependencies(data, keys, variable)
+    data = extract_data(benchmarks, group, variable, measure)
+    
+#    pp.pprint(data)
 
-    print_benchmarks(data, keys)
+    print_benchmarks(data, group, variable, measure)
 
     exit(0)
 
