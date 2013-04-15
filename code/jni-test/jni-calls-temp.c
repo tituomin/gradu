1edc09c01f935a82b23436c013ba5bd52350579a
Timo Tuominen <tituomin@gmail.com>
2013-04-15 13:44:11 +0300

    Misc notes and stats.

--- code/jni-test/jni-calls-temp.c
+++ code/jni-test/jni-calls-temp.c
@@ -1,15 +1,115 @@
 
 /* puuttuu!!! makroversiot...) */
 
+GET_STATIC_TYPE_FIELD(jobject, Object, true);
+GET_STATIC_TYPE_FIELD(jboolean, Boolean, false);
+GET_STATIC_TYPE_FIELD(jbyte, Byte, false);
+GET_STATIC_TYPE_FIELD(jchar, Char, false);
+GET_STATIC_TYPE_FIELD(jshort, Short, false);
+GET_STATIC_TYPE_FIELD(jint, Int, false);
+GET_STATIC_TYPE_FIELD(jlong, Long, false);
+GET_STATIC_TYPE_FIELD(jfloat, Float, false);
+GET_STATIC_TYPE_FIELD(jdouble, Double, false);
+SET_STATIC_TYPE_FIELD(jobject, Object*, Object, true);
+SET_STATIC_TYPE_FIELD(jboolean, bool, Boolean, false);
+SET_STATIC_TYPE_FIELD(jbyte, s1, Byte, false);
+SET_STATIC_TYPE_FIELD(jchar, u2, Char, false);
+SET_STATIC_TYPE_FIELD(jshort, s2, Short, false);
+SET_STATIC_TYPE_FIELD(jint, s4, Int, false);
+SET_STATIC_TYPE_FIELD(jlong, s8, Long, false);
+SET_STATIC_TYPE_FIELD(jfloat, float, Float, false);
+SET_STATIC_TYPE_FIELD(jdouble, double, Double, false);
+GET_TYPE_FIELD(jobject, Object, true);
+GET_TYPE_FIELD(jboolean, Boolean, false);
+GET_TYPE_FIELD(jbyte, Byte, false);
+GET_TYPE_FIELD(jchar, Char, false);
+GET_TYPE_FIELD(jshort, Short, false);
+GET_TYPE_FIELD(jint, Int, false);
+GET_TYPE_FIELD(jlong, Long, false);
+GET_TYPE_FIELD(jfloat, Float, false);
+GET_TYPE_FIELD(jdouble, Double, false);
+SET_TYPE_FIELD(jobject, Object*, Object, true);
+SET_TYPE_FIELD(jboolean, bool, Boolean, false);
+SET_TYPE_FIELD(jbyte, s1, Byte, false);
+SET_TYPE_FIELD(jchar, u2, Char, false);
+SET_TYPE_FIELD(jshort, s2, Short, false);
+SET_TYPE_FIELD(jint, s4, Int, false);
+SET_TYPE_FIELD(jlong, s8, Long, false);
+SET_TYPE_FIELD(jfloat, float, Float, false);
+SET_TYPE_FIELD(jdouble, double, Double, false);
+CALL_VIRTUAL(jobject, Object, NULL, (jobject) result.l, true);
+CALL_VIRTUAL(jboolean, Boolean, 0, result.z, false);
+CALL_VIRTUAL(jbyte, Byte, 0, result.b, false);
+CALL_VIRTUAL(jchar, Char, 0, result.c, false);
+CALL_VIRTUAL(jshort, Short, 0, result.s, false);
+CALL_VIRTUAL(jint, Int, 0, result.i, false);
+CALL_VIRTUAL(jlong, Long, 0, result.j, false);
+CALL_VIRTUAL(jfloat, Float, 0.0f, result.f, false);
+CALL_VIRTUAL(jdouble, Double, 0.0, result.d, false);
+CALL_VIRTUAL(void, Void, , , false);
+CALL_NONVIRTUAL(jobject, Object, NULL, (jobject) result.l, true);
+CALL_NONVIRTUAL(jboolean, Boolean, 0, result.z, false);
+CALL_NONVIRTUAL(jbyte, Byte, 0, result.b, false);
+CALL_NONVIRTUAL(jchar, Char, 0, result.c, false);
+CALL_NONVIRTUAL(jshort, Short, 0, result.s, false);
+CALL_NONVIRTUAL(jint, Int, 0, result.i, false);
+CALL_NONVIRTUAL(jlong, Long, 0, result.j, false);
+CALL_NONVIRTUAL(jfloat, Float, 0.0f, result.f, false);
+CALL_NONVIRTUAL(jdouble, Double, 0.0, result.d, false);
+CALL_NONVIRTUAL(void, Void, , , false);
+CALL_STATIC(jobject, Object, NULL, (jobject) result.l, true);
+CALL_STATIC(jboolean, Boolean, 0, result.z, false);
+CALL_STATIC(jbyte, Byte, 0, result.b, false);
+CALL_STATIC(jchar, Char, 0, result.c, false);
+CALL_STATIC(jshort, Short, 0, result.s, false);
+CALL_STATIC(jint, Int, 0, result.i, false);
+CALL_STATIC(jlong, Long, 0, result.j, false);
+CALL_STATIC(jfloat, Float, 0.0f, result.f, false);
+CALL_STATIC(jdouble, Double, 0.0, result.d, false);
+CALL_STATIC(void, Void, , , false);
+NEW_PRIMITIVE_ARRAY(jbooleanArray, Boolean, 'Z');
+NEW_PRIMITIVE_ARRAY(jbyteArray, Byte, 'B');
+NEW_PRIMITIVE_ARRAY(jcharArray, Char, 'C');
+NEW_PRIMITIVE_ARRAY(jshortArray, Short, 'S');
+NEW_PRIMITIVE_ARRAY(jintArray, Int, 'I');
+NEW_PRIMITIVE_ARRAY(jlongArray, Long, 'J');
+NEW_PRIMITIVE_ARRAY(jfloatArray, Float, 'F');
+NEW_PRIMITIVE_ARRAY(jdoubleArray, Double, 'D');
+
+    GET_PRIMITIVE_ARRAY_ELEMENTS(_ctype, _jname);                           \
+    RELEASE_PRIMITIVE_ARRAY_ELEMENTS(_ctype, _jname);                       \
+    GET_PRIMITIVE_ARRAY_REGION(_ctype, _jname);                             \
+    SET_PRIMITIVE_ARRAY_REGION(_ctype, _jname);
+ 
+
+PRIMITIVE_ARRAY_FUNCTIONS(jboolean, Boolean);
+PRIMITIVE_ARRAY_FUNCTIONS(jbyte, Byte);
+PRIMITIVE_ARRAY_FUNCTIONS(jchar, Char);
+PRIMITIVE_ARRAY_FUNCTIONS(jshort, Short);
+PRIMITIVE_ARRAY_FUNCTIONS(jint, Int);
+PRIMITIVE_ARRAY_FUNCTIONS(jlong, Long);
+PRIMITIVE_ARRAY_FUNCTIONS(jfloat, Float);
+PRIMITIVE_ARRAY_FUNCTIONS(jdouble, Double);
+
+
+/* TODO */
+// todo: impossible to make generic -> examples or generate intelligently?
+// "make database of classes and objects" ..
+// or: reflect and get the object fields etc before measuring
+jobjectIn = (*env)->NewObject(env, jclass jclazz, jmethodID methodID, ...);
+jobjectIn = (*env)->NewObjectV(env, jclass jclazz, jmethodID methodID, va_list args);
+jobjectIn = (*env)->NewObjectA(env, jclass jclazz, jmethodID methodID, jvalue* args);
+
+
 /* OK arrays, strings */
 
 jsizeIn = (*env)->GetArrayLength(env, jarray jarr);
 jsizeIn = (*env)->GetStringLength(env, jstring jstr);
 jsizeIn = (*env)->GetStringUTFLength(env, jstring jstr);
 
-const char_ptrIn = (*env)->GetStringUTFChars(env, jstring jstr, jboolean* isCopy);
-const jchar_ptrIn = (*env)->GetStringChars(env, jstring jstr, jboolean* isCopy);
-const jchar_ptrIn = (*env)->GetStringCritical(env, jstring jstr, jboolean* isCopy);
+char_ptrIn = (*env)->GetStringUTFChars(env, jstring jstr, jboolean* isCopy);
+jchar_ptrIn = (*env)->GetStringChars(env, jstring jstr, jboolean* isCopy);
+jchar_ptrIn = (*env)->GetStringCritical(env, jstring jstr, jboolean* isCopy);
 (*env)->ReleaseStringChars(env, jstring jstr, const jchar* chars);
 (*env)->ReleaseStringCritical(env, jstring jstr, const jchar* carray);
 (*env)->ReleaseStringUTFChars(env, jstring jstr, const char* utf);
@@ -22,9 +122,10 @@ void*In = (*env)->GetPrimitiveArrayCritical(env, jarray jarr, jboolean* isCopy);
 jobjectIn = (*env)->GetObjectArrayElement(env, jobjectArray jarr, jsize index);
 (*env)->SetObjectArrayElement(env, jobjectArray jarr, jsize index, jobject jobj);
 
-/* nio */
+/* OK nio */
 jlongIn = (*env)->GetDirectBufferCapacity(env, jobject jbuf);
 void_ptrIn = (*env)->GetDirectBufferAddress(env, jobject jbuf);
+
 
 /* OK id search */
 
@@ -44,14 +145,8 @@ jstringIn = (*env)->NewString(env, const jchar* unicodeChars, jsize len);
 jobjectArrayIn = (*env)->NewObjectArray(env, jsize length, jclass jelementClass, jobject jinitialElement);
 jobjectIn = (*env)->NewDirectByteBuffer(env, void* address, jlong capacity);
 
-// todo: impossible to make generic -> examples or generate intelligently?
-// "make database of classes and objects" ..
-// or: reflect and get the object fields etc before measuring
-jobjectIn = (*env)->NewObject(env, jclass jclazz, jmethodID methodID, ...);
-jobjectIn = (*env)->NewObjectV(env, jclass jclazz, jmethodID methodID, va_list args);
-jobjectIn = (*env)->NewObjectA(env, jclass jclazz, jmethodID methodID, jvalue* args);
 
-/* references */
+/* OK references */
 jobjectIn = (*env)->NewGlobalRef(env, jobject jobj);
 (*env)->DeleteGlobalRef(env, jobject jglobalRef);
 
@@ -67,11 +162,23 @@ jobjectIn = (*env)->PopLocalFrame(env, jobject jresult);
 jintIn = (*env)->EnsureLocalCapacity(env, jint capacity); // todo: how to test
 jobjectRefTypeIn = (*env)->GetObjectRefType(env, jobject jobj); // todo: ignore?
 
+/* OK exceptions */
+
+jintIn = (*env)->Throw(env, jthrowable jobj);
+jintIn = (*env)->ThrowNew(env, jclass jclazz, const char* message);
+jbooleanIn = (*env)->ExceptionCheck(env);
+jthrowableIn = (*env)->ExceptionOccurred(env);
+(*env)->ExceptionClear(env);
+(*env)->ExceptionDescribe(env);
+(*env)->FatalError(env, const char* msg);
+
+
+/* ---------------- ignore line ------------- */
+
 /* register */
 
 jintIn = (*env)->RegisterNatives(env, jclass jclazz, const JNINativeMethod* methods, jint nMethods);
 jintIn = (*env)->UnregisterNatives(env, jclass jclazz);
-
 
 /* reflection */
 jfieldIDIn = (*env)->FromReflectedField(env, jobject jfield);
@@ -85,16 +192,6 @@ jbooleanIn = (*env)->IsInstanceOf(env, jobject jobj, jclass jclazz);
 jbooleanIn = (*env)->IsSameObject(env, jobject jref1, jobject jref2);
 jclassIn = (*env)->GetSuperclass(env, jclass jclazz);
 
-/* exceptions */
-
-jintIn = (*env)->Throw(env, jthrowable jobj);
-jintIn = (*env)->ThrowNew(env, jclass jclazz, const char* message);
-jbooleanIn = (*env)->ExceptionCheck(env);
-jthrowableIn = (*env)->ExceptionOccurred(env);
-(*env)->ExceptionClear(env);
-(*env)->ExceptionDescribe(env);
-(*env)->FatalError(env, const char* msg);
-
 /* threads */
 
 jintIn = (*env)->MonitorEnter(env, jobject jobj);
@@ -107,14 +204,12 @@ jintIn = (*env)->DetachCurrentThread (JavaVM* vm);
 /* create class */
 jclassIn = (*env)->DefineClass(env, const char *name, jobject loader, const jbyte* buf, jsize bufLen);
 
-
 /* environment */
 
 jintIn = (*env)->DestroyJavaVM (JavaVM* vm);
 jintIn = (*env)->GetJavaVM(env, JavaVM** vm);
 jintIn = (*env)->GetVersion(env);
 jintIn = (*env)->GetEnv (JavaVM* vm, void** env, jint version);
-
 
 /* todo */
 
@@ -127,6 +222,7 @@ jintIn = (*env)->GetEnv (JavaVM* vm, void** env, jint version);
 /* CALL_VIRTUAL(_ctype, _jname, _retfail, _retok, _isref) */
 /* CALL_NONVIRTUAL(_ctype, _jname, _retfail, _retok, _isref) */
 /* CALL_STATIC(_ctype, _jname, _retfail, _retok, _isref) */
+
 
 /* NEW_PRIMITIVE_ARRAY(_artype, _jname, _typechar) */
 
